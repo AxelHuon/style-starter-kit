a7d7fdb87453bab18f2801a9df4f4f3af6308712
AxelHuon <81225848+AxelHuon@users.noreply.github.com>
2023-11-16 21:53:22 +0100

    change npm-publish.yml

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { initialisationColors } from "./colors/index.js";
 
 const args = process.argv.slice(2);
 
-if (args[0] === "colors") {
+if (args[0] === "init-colors") {
   initialisationColors();
 }
 
