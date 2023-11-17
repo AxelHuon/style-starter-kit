f0c9b1abe04c59d381ab337eb4057d730de28217
AxelHuon <81225848+AxelHuon@users.noreply.github.com>
2023-11-17 11:52:12 +0100

    [feature/init-lib-command][feat]: add init command

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 
 import { initialisationColors } from "./colors/index.js";
+import { initLib } from "./init/index.js";
 
 const args = process.argv.slice(2);
 
-if (args[0] === "init-colors") {
+if (args[0] === "init") {
+  initLib()
+}else if (args[0] === "init-colors"){
   initialisationColors();
 }
 
