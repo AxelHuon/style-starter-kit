9bbba14630dc6bbf852362f6afd6ba47fac59f24
Diego Da Silva M <78415421+diedasilva@users.noreply.github.com>
2023-11-19 19:46:05 +0100

    update_fonts
    We must delete 'index.js'

--- src/fonts/index.ts
+++ src/fonts/index.ts
@@ -1,63 +1,30 @@
-var __awaiter =
-  (this && this.__awaiter) ||
-  function (thisArg, _arguments, P, generator) {
-    function adopt(value) {
-      return value instanceof P
-        ? value
-        : new P(function (resolve) {
-            resolve(value);
-          });
-    }
-    return new (P || (P = Promise))(function (resolve, reject) {
-      function fulfilled(value) {
-        try {
-          step(generator.next(value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function rejected(value) {
-        try {
-          step(generator["throw"](value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function step(result) {
-        result.done
-          ? resolve(result.value)
-          : adopt(result.value).then(fulfilled, rejected);
-      }
-      step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-  };
 import inquirer from "inquirer";
 import fetch from "node-fetch";
 import fs from "fs";
 import path from "path";
-const FONTS = [
+
+const FONTS: string[] = [
   "Roboto",
   "Open Sans",
   "Lato",
   // ... ajoutez d'autres polices si vous souhaitez les supporter
 ];
-function selectFont() {
-  return __awaiter(this, void 0, void 0, function* () {
-    const questions = [
-      {
-        type: "list",
-        name: "fontFamily",
-        message: "Quelle police souhaitez-vous télécharger?",
-        choices: FONTS,
-        default: FONTS[0],
-      },
-    ];
-    const answers = yield inquirer.prompt(questions);
-    return answers.fontFamily;
-  });
+
+async function selectFont(): Promise<string> {
+  const questions = [
+    {
+      type: "list",
+      name: "fontFamily",
+      message: "Quelle police souhaitez-vous télécharger?",
+      choices: FONTS,
+      default: FONTS[0],
+    },
+  ];
+  const answers = await inquirer.prompt(questions);
+  return answers.fontFamily;
 }
 
-async function downloadFontFamily(fontFamily) {
+async function downloadFontFamily(fontFamily: string): Promise<void> {
   const fontUrl = `https://fonts.googleapis.com/css2?family=${encodeURIComponent(
     fontFamily
   )}:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap`;
@@ -70,9 +37,9 @@ async function downloadFontFamily(fontFamily) {
       );
     }
 
-    const fontCss = await response.text();
-    const fontFaceSections = fontCss.split(/@font-face\s*\{/);
-    let newFontCss = fontCss;
+    const fontCss: string = await response.text();
+    const fontFaceSections: string[] = fontCss.split(/@font-face\s*\{/);
+    let newFontCss: string = fontCss;
 
     if (fontCss) {
       console.log(
@@ -84,19 +51,19 @@ async function downloadFontFamily(fontFamily) {
         const styleMatch = section.match(/font-style:\s*(\w+)/);
 
         if (urlMatch && weightMatch && styleMatch) {
-          const url = urlMatch[1];
-          const weight = weightMatch[1];
-          const style = styleMatch[1];
-          const fileExtension = path.extname(url);
+          const url: string = urlMatch[1];
+          const weight: string = weightMatch[1];
+          const style: string = styleMatch[1];
+          const fileExtension: string = path.extname(url);
 
-          const fontDirectory = path.join("./public/fonts", fontFamily);
+          const fontDirectory: string = path.join("./public/fonts", fontFamily);
           if (!fs.existsSync(fontDirectory)) {
             fs.mkdirSync(fontDirectory, { recursive: true });
           }
 
-          const fileName = `${fontFamily}_${style}_${weight}`;
+          const fileName: string = `${fontFamily}_${style}_${weight}`;
 
-          let newUrl = path.join(fontDirectory, fileName);
+          let newUrl: string = path.join(fontDirectory, fileName);
           newUrl = "'" + newUrl + fileExtension + "'";
           newUrl = newUrl.replace(/\\/g, "/");
 
@@ -105,8 +72,8 @@ async function downloadFontFamily(fontFamily) {
           console.log(newFontCss);
         }
       }
-      const fontsCssPath = "./src/styles/fonts.css";
-      const existingContent = fs.readFileSync(fontsCssPath, "utf8");
+      const fontsCssPath: string = "./src/styles/fonts.css";
+      const existingContent: string = fs.readFileSync(fontsCssPath, "utf8");
       fs.writeFileSync(fontsCssPath, newFontCss + existingContent);
 
       console.log(
@@ -116,41 +83,42 @@ async function downloadFontFamily(fontFamily) {
     } else {
       throw new Error("Aucune URL de police trouvée dans le CSS fourni.");
     }
-  } catch (error) {
+  } catch (error: any) {
     console.error(
       `Erreur lors du téléchargement de la police ${fontFamily}: ${error.message}`
     );
   }
 }
 
-async function downloadFontFile(url, directory, fileName) {
+async function downloadFontFile(url: string, directory: string, fileName: string): Promise<void> {
   console.log(`Téléchargement de ${url} ...`);
   try {
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`Échec du téléchargement de l'URL : ${url}`);
     }
-    const buffer = await response.buffer();
+    const buffer: Buffer = await response.buffer();
 
-    const fileExtension = path.extname(url);
-    const savePath = path.join(directory, `${fileName}_family${fileExtension}`);
+    const fileExtension: string = path.extname(url);
+    const savePath: string = path.join(directory, `${fileName}_family${fileExtension}`);
 
     fs.writeFileSync(savePath, buffer);
     console.log(`Téléchargement réussi et sauvegardé à ${savePath}`);
-  } catch (error) {
+  } catch (error: any) {
     console.error(
       `Erreur lors du téléchargement du fichier de police : ${error.message}`
     );
   }
 }
 
-async function initializeLibrary() {
+async function initializeLibrary(): Promise<void> {
   try {
-    const selectedFont = await selectFont();
+    const selectedFont: string = await selectFont();
     console.log(`Vous avez choisi de télécharger la police : ${selectedFont}`);
     await downloadFontFamily(selectedFont);
-  } catch (error) {
+  } catch (error: any) {
     console.error(`Erreur lors de l'initialisation : ${error.message}`);
   }
 }
+
 initializeLibrary();
